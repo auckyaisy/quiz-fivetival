e1a622e98cf90854edbaa9dab259ee3708d92a1e
Aucky Aisy <auckyqq@gmail.com>
2022-09-03 08:40:11 +0700

    input user

--- inputtimeq.py
+++ inputtimeq.py
@@ -1,26 +1,26 @@
 
-from quiz.models import User, Team
-from django.conf import settings
-from django.contrib.auth import get_user_model
-from django.contrib.auth.hashers import make_password
-from django.contrib.auth.models import User
-from django.contrib import admin
-from django.contrib.auth import authenticate
-import django
-import csv
-import sys
-import os
-import django
+import csv, sys, os, django
 
 
 project_dir = "/"
 sys.path.append(project_dir)
 os.environ['DJANGO_SETTINGS_MODULE'] = 'quizfivetival.settings'
 # os.environ.setdefault("DJANGO_SETTINGS_MODULE", __file__)
+import django
 django.setup()
 
 
-# User = get_user_model()
+from django.contrib.auth import authenticate
+from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.auth.hashers import make_password
+
+
+from django.contrib.auth import get_user_model
+
+from django.contrib.auth.hashers import make_password
+from django.conf import settings
+from quiz.models import User, Team
 
 file = 'eq.csv'
 
